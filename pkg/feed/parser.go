68a6c73ce11c853e3b52af46c916edb6919a2bf9
toga4 <81744248+toga4@users.noreply.github.com>
2022-01-25 22:16:56 +0900

    Stop sharing parsers between goroutine

--- pkg/feed/parser.go
+++ pkg/feed/parser.go
@@ -7,18 +7,17 @@ import (
 )
 
 type Parser struct {
-	parser *gofeed.Parser
 	mapper ParseItemMapper
 }
 
 func NewParser() *Parser {
-	parser := gofeed.NewParser()
 	mapper := &defaultParseItemMapper{}
-	return &Parser{parser, mapper}
+	return &Parser{mapper}
 }
 
 func (p *Parser) ParseURL(ctx context.Context, url string) ([]*Item, error) {
-	feed, err := p.parser.ParseURLWithContext(url, ctx)
+	parser := gofeed.NewParser()
+	feed, err := parser.ParseURLWithContext(url, ctx)
 	if err != nil {
 		return nil, err
 	}
